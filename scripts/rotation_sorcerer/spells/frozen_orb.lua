312836b2d9a1ff0588265a09888de0c7bdb66172
qqtnn <qqtn@proton.me>
2024-07-17 19:53:29 +0200

    rotation sorcerer added frozen orb elite filter

--- scripts/rotation_sorcerer/spells/frozen_orb.lua
+++ scripts/rotation_sorcerer/spells/frozen_orb.lua
@@ -3,13 +3,15 @@ local my_utility = require("my_utility/my_utility")
 local frozen_orb_menu_elements =
 {
     tree_tab            = tree_node:new(1),
-    main_boolean        = checkbox:new(true, get_hash(my_utility.plugin_label .. "spark_main_boolean")),
+    main_boolean        = checkbox:new(true, get_hash(my_utility.plugin_label .. "frozen_orb_main_boolean")),
+    elite_filter        = checkbox:new(false, get_hash(my_utility.plugin_label .. "frozen_orb_elite_filter")),
 }
 
 local function menu()
     
     if frozen_orb_menu_elements.tree_tab:push("Frozen Orb")then
         frozen_orb_menu_elements.main_boolean:render("Enable Spell", "")
+        frozen_orb_menu_elements.elite_filter:render("Elite Filter", "")
  
         frozen_orb_menu_elements.tree_tab:pop()
     end
@@ -43,19 +45,21 @@ local function logics(target)
         return false;
     end;
 
-    -- local player_local = get_local_player();
-    if not target:is_boss() and not target:is_elite() and not target:is_champion() then
-        return false;
+    if frozen_orb_menu_elements.elite_filter:get() then
+        -- local player_local = get_local_player();
+        if not target:is_boss() and not target:is_elite() and not target:is_champion() then
+            return false;
+        end
     end
     
     local player_position = get_player_position();
     local target_position = target:get_position();
-    local is_collision = prediction.is_wall_collision(player_position, target_position, 1.5)
+    local is_collision = prediction.is_wall_collision(player_position, target_position, 1.0)
     if is_collision then
         return false
     end
 
-    if cast_spell.target(target, frozen_orb_data, false) then
+    if cast_spell.position(spell_id_fozen_orb, target_position:get_extended(player_position, -0.5), 0.4) then
 
         local current_time = get_time_since_inject();
         next_time_allowed_cast = current_time + 0.4;
