6e7652f57707e19cd02be7d11182e1b7e1dbce60
qqtnn <qqtn@proton.me>
2024-07-20 13:43:07 +0200

    rotation rogue improvements on spell barrage

--- scripts/rotation_rogue/spells/barrage.lua
+++ scripts/rotation_rogue/spells/barrage.lua
@@ -4,13 +4,17 @@ local menu_elements_barrage_base =
 {
     tree_tab            = tree_node:new(1),
     main_boolean        = checkbox:new(true, get_hash(my_utility.plugin_label .. "barrage_base_main_bool")),
+    use_combo_points    = checkbox:new(false, get_hash(my_utility.plugin_label .. "barrage_use_combo_points")),
+    combo_points_slider = slider_int:new(0, 6, 0, get_hash(my_utility.plugin_label .. "barrage__min_combo_points")),
 }
 
 local function menu()
-    
-    if menu_elements_barrage_base.tree_tab:push("Barrage")then
+    if menu_elements_barrage_base.tree_tab:push("Barrage") then
         menu_elements_barrage_base.main_boolean:render("Enable Spell", "")
- 
+        menu_elements_barrage_base.use_combo_points:render("Use Combo Points", "")
+        if menu_elements_barrage_base.use_combo_points:get() then
+            menu_elements_barrage_base.combo_points_slider:render("Min Combo Points", "")
+        end
         menu_elements_barrage_base.tree_tab:pop()
     end
 end
@@ -22,14 +26,15 @@ local spell_data_barrage = spell_data:new(
     9.0,                        -- range
     1.5,                        -- cast_delay
     3.0,                        -- projectile_speed
-    true,                      -- has_collision
+    true,                       -- has_collision
     spell_id_barrage,           -- spell_id
     spell_geometry.rectangular, -- geometry_type
-    targeting_type.skillshot    --targeting_type
+    targeting_type.skillshot    -- targeting_type
 )
+
 local next_time_allowed_cast = 0.0;
+
 local function logics(target)
-    
     local menu_boolean = menu_elements_barrage_base.main_boolean:get();
     local is_logic_allowed = my_utility.is_spell_allowed(
                 menu_boolean, 
@@ -41,22 +46,27 @@ local function logics(target)
     end;
 
     local player_local = get_local_player();
+
+    if menu_elements_barrage_base.use_combo_points:get() then
+        local combo_points = player_local:get_rogue_combo_points()
+        local min_combo_points = menu_elements_barrage_base.combo_points_slider:get()
+        if combo_points < min_combo_points then
+            return false
+        end
+    end
     
     local player_position = get_player_position();
     local target_position = target:get_position();
 
     if cast_spell.target(target, spell_data_barrage, false) then
-
         local current_time = get_time_since_inject();
         next_time_allowed_cast = current_time + 0.9;
-
-        console.print("Rouge, Casted Barrage");
+        console.print("Rogue, Casted Barrage");
         return true;
     end;
             
     return false;
 end
-
 
 return 
 {
