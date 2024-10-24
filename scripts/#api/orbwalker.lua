a2bf2474c5f2b06b14f56198ba4b858191a552ca
qqtnn <qqtn@proton.me>
2024-10-24 11:33:14 +0200

    lua api added orbwalker functions setters

--- scripts/#api/orbwalker.lua
+++ scripts/#api/orbwalker.lua
@@ -14,3 +14,15 @@ orbwalker = {}
 --- Gets the current orbwalker mode.
 --- @return orb_mode The current orbwalker mode.
 function orbwalker.get_orb_mode() end
+
+--- @return nil
+--- @param mode orb_mode | number
+function orbwalker.set_orbwalker_mode(mode) end
+
+--- @return nil
+--- @param value boolean
+function orbwalker.set_block_movement(value) end
+
+--- @return nil
+--- @param value boolean
+function orbwalker.set_auto_loot_toggle(value) end
